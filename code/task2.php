19da02b6a016ec7cea685c891b99640921e842b0
Илья Букарев <ilyabej02@gmail.com>
2022-10-06 17:39:56 +0200

    task2

---
+++ code/task2.php
@@ -0,0 +1,14 @@
+<?php
+
+//Task 2
+$m1 = 10;
+echo $m1 . "<br>";
+$m2 = 1001;
+echo $m2;
+$m3 = 123.456;
+echo $m3 . "<br>";
+echo (6+6) . "<br>";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo ($last_month - $this_month);
